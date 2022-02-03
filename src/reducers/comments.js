1536f37d91168dbd8bbe36457c4dc67ca9bed60b
chucoyos <facturasmanzanillo@hotmail.com>
2022-02-02 22:17:06 -0600

    set actions and save_comment reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,9 @@
+import { SAVE_COMMENT } from 'actions/types';
+
 export default function comments(state = [], action) {
 	switch (action.type) {
+		case SAVE_COMMENT:
+			return [...state, action.payload];
 		default:
 			return state;
 	}
