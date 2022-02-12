d7a6cac6c016e3ff267ace79ec0c1694e5fe08d2
chucoyos <facturasmanzanillo@hotmail.com>
2022-02-12 14:44:28 -0600

    fetch comments from an external api

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ children, initialState = {} }) => {
-	return (
-		<Provider store={createStore(reducers, initialState)}>{children}</Provider>
+	const store = createStore(
+		reducers,
+		initialState,
+		applyMiddleware(reduxPromise)
 	);
+	return <Provider store={store}>{children}</Provider>;
 };
