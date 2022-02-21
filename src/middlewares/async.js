a233e91e882cd70413ba04c4a1192c17e70e4572
chucoyos <facturasmanzanillo@hotmail.com>
2022-02-21 12:28:31 -0600

    add async middleware

---
+++ src/middlewares/async.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ dispatch }) =>
+	(next) =>
+	(action) => {
+		if (!action.payload || !action.payload.then) {
+			return next(action);
+		}
+
+		action.payload.then(function (response) {
+			const newAction = { ...action, payload: response };
+			dispatch(newAction);
+		});
+	};
